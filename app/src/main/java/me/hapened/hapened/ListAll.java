5eeb5c244e8de4317df3a0fe0ea350f677b042f3
S. Shen <slucy6814@gmail.com>
2015-10-07 16:11:46 -0400

    random

--- app/src/main/java/me/hapened/hapened/ListAll.java
+++ app/src/main/java/me/hapened/hapened/ListAll.java
@@ -1,42 +1,22 @@
 package me.hapened.hapened;
 
-import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class LoadScreen extends ActionBarActivity {
-
+public class ListAll extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_load_screen);
-
-        Thread welcomeThread = new Thread() {
-
-            @Override
-            public void run() {
-                try {
-                    super.run();
-                    sleep(10000);  //Delay of 10 seconds
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                } finally {
-
-                    //Intent i = new Intent(LoadScreen.this, .class);
-                    //startActivity(i);
-                    LoadScreen.this.finish();
-                }
-            }
-        };
-        welcomeThread.start();
+        setContentView(R.layout.activity_list_all);
     }
 
-
-/*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_load_screen, menu);
+        getMenuInflater().inflate(R.menu.menu_list_all, menu);
         return true;
     }
 
@@ -53,5 +33,5 @@ public class LoadScreen extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }*/
+    }
 }
