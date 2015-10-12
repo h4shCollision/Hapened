9f52f0e64b969468a10edcf0c20729f140f36b8d
S. Shen <slucy6814@gmail.com>
2015-10-12 17:23:46 -0400

    FM method signatures

--- app/src/main/java/me/hapened/hapened/FileManager.java
+++ app/src/main/java/me/hapened/hapened/FileManager.java
@@ -4,5 +4,34 @@ package me.hapened.hapened;
  * Created by ellens on 10/12/15.
  */
 public class FileManager {
-    
+    private static FileManager instance;
+
+    public static synchronized FileManager getInstance(){
+        if (instance==null){
+            instance=new FileManager();
+        }
+        return instance;
+    }
+
+    private FileManager(){
+        //get list
+
+    }
+
+    public void getItem(int index){
+
+    }
+
+    public void addItem(int index,String s){
+
+    }
+
+    public void setItem(int index,String s){
+
+    }
+
+    public void save(){
+
+    }
+
 }
